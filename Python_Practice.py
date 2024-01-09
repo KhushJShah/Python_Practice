b40a2f7ffbef2ac58e3f46639bcf4ae52249a40b
KhushJShah <khushjayant.shah@gwmail.gwu.edu>
2024-01-08 20:11:15 -0500

    Update

--- Python_Practice.py
+++ Python_Practice.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
 #%%
 '''
-In Python, a string can be split on a delimiter.
+Q. In Python, a string can be split on a delimiter.
 
 Example:
 
@@ -74,3 +74,265 @@ if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
     print(result)
+
+#%%
+"""
+Q. You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python.
+
+Function Description
+
+Complete the print_full_name function in the editor below.
+
+print_full_name has the following parameters:
+
+string first: the first name
+string last: the last name
+Prints
+
+string: 'Hello  ! You just delved into python' where  and  are replaced with  and .
+Input Format
+
+The first line contains the first name, and the second line contains the last name.
+
+Constraints
+
+The length of the first and last names are each ≤ .
+
+Sample Input 0
+
+Ross
+Taylor
+Sample Output 0
+
+Hello Ross Taylor! You just delved into python.
+Explanation 0
+
+The input read by the program is stored as a string data type. A string is a collection of characters.
+"""
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+    return print("Hello "+first+ " " + last + "! You just delved into python.") 
+    
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+
+#%%
+"""
+Q. We have seen that lists are mutable (they can be changed), and tuples are immutable (they cannot be changed).
+
+Let's try to understand this with an example.
+
+You are given an immutable string, and you want to make changes to it.
+
+Example
+
+>>> string = "abracadabra"
+You can access an index by:
+
+>>> print string[5]
+a
+What if you would like to assign a value?
+
+>>> string[5] = 'k' 
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'str' object does not support item assignment
+How would you approach this?
+
+One solution is to convert the string to a list and then change the value.
+Example
+
+>>> string = "abracadabra"
+>>> l = list(string)
+>>> l[5] = 'k'
+>>> string = ''.join(l)
+>>> print string
+abrackdabra
+Another approach is to slice the string and join it back.
+Example
+
+>>> string = string[:5] + "k" + string[6:]
+>>> print string
+abrackdabra
+Task
+Read a given string, change the character at a given index and then print the modified string.
+Function Description
+
+Complete the mutate_string function in the editor below.
+
+mutate_string has the following parameters:
+
+string string: the string to change
+int position: the index to insert the character at
+string character: the character to insert
+Returns
+
+string: the altered string
+Input Format
+
+The first line contains a string, .
+The next line contains an integer , the index location and a string , separated by a space.
+
+Sample Input
+
+STDIN           Function
+-----           --------
+abracadabra     s = 'abracadabra'
+5 k             position = 5, character = 'k'
+Sample Output
+
+abrackdabra
+"""
+def mutate_string(string, position, character):
+    string = string[:position] + character + string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
+#%%
+"""
+Q. In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+
+NOTE: String letters are case-sensitive.
+
+Input Format
+
+The first line of input contains the original string. The next line contains the substring.
+
+Constraints
+
+
+Each character in the string is an ascii character.
+
+Output Format
+
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+Sample Input
+
+ABCDCDC
+CDC
+Sample Output
+
+2
+Concept
+
+Some string processing examples, such as these, might be useful.
+There are a couple of new concepts:
+In Python, the length of a string is found by the function len(s), where  is the string.
+To traverse through the length of a string, use a for loop:
+
+for i in range(0, len(s)):
+    print (s[i])
+A range function is used to loop over some length:
+
+range (0, 5)
+"""
+def count_substring(string, sub_string):
+    i = string.find(sub_string)
+    if i == -1:
+        return 0
+    return 1 + count_substring(string[i + 1:], sub_string)
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
+
+# The code iteratively checks for multipe occurences.
+
+#%%
+"""
+Q. Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+
+str.isalnum()
+This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+
+>>> print 'ab123'.isalnum()
+True
+>>> print 'ab123#'.isalnum()
+False
+str.isalpha()
+This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+
+>>> print 'abcD'.isalpha()
+True
+>>> print 'abcd1'.isalpha()
+False
+str.isdigit()
+This method checks if all the characters of a string are digits (0-9).
+
+>>> print '1234'.isdigit()
+True
+>>> print '123edsd'.isdigit()
+False
+str.islower()
+This method checks if all the characters of a string are lowercase characters (a-z).
+
+>>> print 'abcd123#'.islower()
+True
+>>> print 'Abcd123#'.islower()
+False
+str.isupper()
+This method checks if all the characters of a string are uppercase characters (A-Z).
+
+>>> print 'ABCD123#'.isupper()
+True
+>>> print 'Abcd123#'.isupper()
+False
+Task
+
+You are given a string .
+Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+Input Format
+
+A single line containing a string .
+
+Constraints
+
+
+Output Format
+
+In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+In the third line, print True if  has any digits. Otherwise, print False.
+In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+
+Sample Input
+
+qA2
+Sample Output
+
+True
+True
+True
+True
+True
+"""
+if __name__ == '__main__':
+    s = input()
+    attrs = ["isalnum", "isalpha", "isdigit", "islower", "isupper"]
+    for attr in attrs:
+        print(any(getattr(i, attr)() for i in s))
