5405617de2a45d823cda86ba12b2ce766c806f78
KhushJShah <khushjayant.shah@gwmail.gwu.edu>
2024-01-28 22:51:55 -0500

    Update 01/28/2024

--- Python_Practice.py
+++ Python_Practice.py
@@ -551,3 +551,55 @@ if __name__ == '__main__':
     print_formatted(n)
 
 #%%
+"""
+Q. You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+
+
+Given a full name, your task is to capitalize the name appropriately.
+
+Input Format
+
+A single line of input containing the full name, .
+
+Constraints
+
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+
+Output Format
+
+Print the capitalized string, .
+
+Sample Input
+
+chris alan
+Sample Output
+
+Chris Alan
+"""
+def solve(s):
+    import string
+    u = string.ascii_uppercase
+    l = string.ascii_lowercase
+
+    words = list(s)
+    for i in range(len(words)):
+        if i == 0 or (words[i-1]==" "):
+            words[i]=words[i].capitalize()
+            result = ''.join(map(str, words))
+    return result   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+"""
+You can definitely utilize s.title() function for the problem, but it does not identify if there are multiple spaces for eg: K l 3g 4k. Thus, the function needs to be updated.
+"""
